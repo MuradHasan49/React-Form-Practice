3428dd315e655b95387dfee79be697c113a54b38
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-08 16:07:51 +0600

    remove extra code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-import OnSubmit from '../Form/OnSubmit'
-import FormAction from '../Form/FormAction'
-import Controlled from '../Form/Controlled'
-import Uncontrolled from '../Form/Uncontrolled'
-import HookForm from '../Form/HookForm'
+
 import Sidebar from '../../Sidebar'
 
 const Home = () => {
@@ -11,7 +6,7 @@ const Home = () => {
     return (
         <>
             <div className="min-h-[85vh] px-10 py-5">
-                <Sidebar/>
+                <Sidebar />
             </div>
         </>
     )
