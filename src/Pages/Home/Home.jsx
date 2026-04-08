8fd2d41bd7173df6b42e81c420ef99fea4ecb20c
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-08 12:03:01 +0600

    setup react porject and install tailwind css , react router and more

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <>
-    <div className="min-h-[85vh]">
+    return (
+        <>
+            <div className="min-h-[85vh]">
 
-    Home
-    </div>
-    </>
-  )
+                
+            </div>
+        </>
+    )
 }
 
 export default Home
