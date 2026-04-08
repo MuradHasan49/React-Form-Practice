4d3af22b2a31d2e24cb889f1eb7f22aa1d0b8a40
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-08 15:20:56 +0600

    modify btn color

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,32 +6,32 @@ const Sidebar = () => {
             <h2 className="text-2xl font-bold text-center mb-8">Sidebar</h2>
 
             <div className="flex flex-col gap-3">
-                {/* OnSubmit Button */}
-                <button className="btn btn-primary justify-start gap-3 shadow-md">
+
+                <button className="btn bg-gray-700  justify-start gap-3 shadow-md">
                     <span className="badge badge-ghost text-xs">01</span>
                     OnSubmit
                 </button>
 
-                {/* FormAction Button */}
-                <button className="btn btn-secondary justify-start gap-3 shadow-md text-white">
+
+                <button className="btn bg-gray-700  justify-start gap-3 shadow-md text-white">
                     <span className="badge badge-ghost text-xs">02</span>
                     FormAction
                 </button>
 
-                {/* Controlled Button */}
-                <button className="btn btn-warning justify-start gap-3 shadow-md text-white">
+
+                <button className="btn bg-gray-700  justify-start gap-3 shadow-md text-white">
                     <span className="badge badge-ghost text-xs">03</span>
                     Controlled
                 </button>
 
-                {/* Uncontrolled Button */}
-                <button className="btn btn-info justify-start gap-3 shadow-md text-white">
+
+                <button className="btn bg-gray-700  justify-start gap-3 shadow-md text-white">
                     <span className="badge badge-ghost text-xs">04</span>
                     Uncontrolled
                 </button>
 
-                {/* HookForm Button */}
-                <button className="btn btn-accent justify-start gap-3 shadow-md text-white">
+
+                <button className="btn bg-gray-700  justify-start gap-3 shadow-md text-white">
                     <span className="badge badge-ghost text-xs">05</span>
                     HookForm
                 </button>
