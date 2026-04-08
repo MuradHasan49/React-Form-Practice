62e672a76de7722109153302c2936f33eb1e4300
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-08 16:10:11 +0600

    add link in navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
     return (
         <>
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to="/">
+                    <a className="btn btn-ghost text-xl">MHS</a>
+                    </Link>
                 </div>
                 <div className="flex-none">
                     <div className="dropdown dropdown-end">
